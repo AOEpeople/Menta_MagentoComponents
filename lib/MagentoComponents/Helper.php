492fd9a42ff978c6292899ed4ae211626e864377
Agata <a.firlejczyk@macopedia.pl>
2014-01-31 14:51:21 +0100

    Added new component - Helper

---
+++ lib/MagentoComponents/Helper.php
@@ -0,0 +1,19 @@
+<?php
+
+class MagentoComponents_Helper extends Menta_Component_AbstractTest
+{
+
+	/**
+	 * Normalize
+	 * @param $text
+	 *
+	 * @return string
+	 */
+	protected function normalize($text, $currency = '$')
+	{
+		$text = str_replace(' ', '', $text);
+		$text = str_replace($currency, '<CURRENCYSYMBOL>', $text);
+		$text = trim($text);
+		return $text;
+	}
+}
