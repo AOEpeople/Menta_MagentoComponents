7c8822564f0a26111b3d10f808de0558cd48139e
Agata <a.firlejczyk@macopedia.pl>
2014-03-17 21:10:47 +0100

    CustomerAccount component - refactoring, add/change methods descriptions

--- lib/MagentoComponents/Pages/CustomerAccount.php
+++ lib/MagentoComponents/Pages/CustomerAccount.php
@@ -2,9 +2,9 @@
 
 class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTest
 {
-
     /**
      * Path for element which is present only on dashboard page
+     *
      * @return string
      */
     public function getDashboardIndicatorPath()
@@ -13,7 +13,8 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
-     * Path for create button on login/register page
+     * Path for create account button on login/register page
+     *
      * @return string
      */
     public function getSplitPageRegistrationButtonPath()
@@ -22,7 +23,18 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
+     * Path for login button on login/register page
+     *
+     * @return string
+     */
+    public function getSplitPageLoginButtonPath()
+    {
+        return '//button[' . Menta_Util_Div::contains($this->__('Login'), 'title') . ']';
+    }
+
+    /**
      * Path for newsletter indicator
+     *
      * @return string
      */
     public function getNewsletterCheckboxIndicatorPath()
@@ -32,6 +44,7 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
 
     /**
      * Path for edit newsletter in customer dashboard page
+     *
      * @return string
      */
     public function getNewsletterEditPathInDashoboard()
@@ -40,10 +53,52 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
         . "][1]//a[" . Menta_Util_Div::containsText($this->__('Edit')) . "]";
     }
 
+    /**
+     * Path for submit registration button on create account page
+     * @return string
+     */
     public function getRegistrationSubmitButtonPath()
     {
         return "//button[@type='submit'][" .
         Menta_Util_Div::contains($this->__('Submit'), 'title') . "]";
+    }
+
+    /**
+     * Login
+     *
+     * @param string $username
+     * @param string $password
+     */
+    public function login($username = NULL, $password = NULL)
+    {
+        if (is_null($username) || is_null($password)) {
+            $username = $this->getConfiguration()->getValue('testing.frontend.user');
+            $password = $this->getConfiguration()->getValue('testing.frontend.password');
+        }
+        $this->openSplitLoginOrRegister();
+
+        $this->getHelperCommon()->type("//input[@id='email']", $username, true, true);
+        $this->getHelperCommon()->type("//input[@id='pass']", $password, true, true);
+
+        Menta_Events::dispatchEvent('MagentoComponents_Pages_CustomerAccount->login:beforeSubmit', array(
+            'component' => $this
+        ));
+
+        $this->getHelperCommon()->click($this->getSplitPageLoginButtonPath());
+
+        $this->getHelperAssert()->assertBodyClass('customer-account-index');
+    }
+
+    /**
+     * Logout
+     */
+    public function logout()
+    {
+        $helper = Menta_ComponentManager::get('MagentoComponents_Helper');
+        /* @var $helper MagentoComponents_Helper */
+        $this->getHelperCommon()->click($helper->getLogoutLinkPath());
+        $this->getHelperWait()->waitForElementPresent("//h1[" . Menta_Util_Div::containsText($this->__('You are now logged out')) . "]");
+        $this->getHelperAssert()->assertElementPresent("//h1[" . Menta_Util_Div::containsText($this->__('You are now logged out')) . "]");
     }
 
     /**
@@ -61,6 +116,15 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
+     * Open registration page
+     */
+    public function openRegistrationPage()
+    {
+        $this->getHelperCommon()->open('/customer/account/create/');
+        $this->getHelperAssert()->assertBodyClass('customer-account-create');
+    }
+
+    /**
      * Got to dashboard
      *
      * @return void
@@ -71,6 +135,9 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
         $this->assertIsOnDashboard();
     }
 
+    /**
+     * Assert user is in My account dashboard
+     */
     public function assertIsOnDashboard()
     {
         $this->getHelperAssert()->assertTitle($this->__('My Account'));
@@ -79,7 +146,7 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
-     * Got to history
+     * Got to order history page
      *
      * @return void
      */
@@ -111,37 +178,6 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
-     * Login
-     *
-     * @param string $username
-     * @param string $password
-     */
-    public function login($username = NULL, $password = NULL)
-    {
-        if (is_null($username) || is_null($password)) {
-            $username = $this->getConfiguration()->getValue('testing.frontend.user');
-            $password = $this->getConfiguration()->getValue('testing.frontend.password');
-        }
-        $this->openSplitLoginOrRegister();
-        //$this->getHelperCommon()->click("//ul[@class='links personal-items']/li[@class='first']/a");
-        $this->getHelperCommon()->type("//input[@id='email']", $username, true, true);
-        $this->getHelperCommon()->type("//input[@id='pass']", $password, true, true);
-
-        Menta_Events::dispatchEvent('MagentoComponents_Pages_CustomerAccount->login:beforeSubmit', array(
-            'component' => $this
-        ));
-
-        $this->getHelperCommon()->click($this->getSplitPageLoginButtonPath());
-
-        $this->getHelperAssert()->assertBodyClass('customer-account-index');
-    }
-
-    public function getSplitPageLoginButtonPath()
-    {
-        return '//button[' . Menta_Util_Div::contains($this->__('Login'), 'title') . ']';
-    }
-
-    /**
      * Got to forgot password page
      *
      * @return void
@@ -152,23 +188,20 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
     }
 
     /**
-     * Logout
-     *
+     * Logout user using direct link
      */
-    public function logout()
-    {
-        /* @var $helper MagentoComponents_Helper */
-        $helper = Menta_ComponentManager::get('MagentoComponents_Helper');
-        $this->getHelperCommon()->click($helper->getLogoutLinkPath());
-        $this->getHelperWait()->waitForElementPresent("//h1[" . Menta_Util_Div::containsText($this->__('You are now logged out')) . "]");
-        $this->getHelperAssert()->assertElementPresent("//h1[" . Menta_Util_Div::containsText($this->__('You are now logged out')) . "]");
-    }
-
     public function logoutViaOpen()
     {
         $this->getHelperCommon()->open('/customer/account/logout/');
     }
 
+    /**
+     * Create new mail address
+     *
+     * @param string $type
+     * @return mixed
+     * @throws Exception
+     */
     public function createNewMailAddress($type = '')
     {
         if (!$this->getConfiguration()->issetKey('testing.newmailaddresspattern')) {
@@ -177,36 +210,33 @@ class MagentoComponents_Pages_CustomerAccount extends Menta_Component_AbstractTe
         $template = $this->getConfiguration()->getValue('testing.newmailaddresspattern');
         $replace = array(
             '###TYPE###' => $type,
-            '###RANDOM###' => $this->createRandomString(4),
+            '###RANDOM###' => Menta_Util_Div::createRandomString(4),
             '###TIME###' => time(),
             '###TESTID###' => $this->getTest()->getTestId()
         );
         return str_replace(array_keys($replace), array_values($replace), $template);
     }
 
+    /**
+     * Create random password
+     *
+     * @param int $length
+     * @return string
+     */
     public function createRandomPassword($length = 8)
     {
         return Menta_Util_Div::createRandomString($length);
     }
 
+    /**
+     * Create random user name
+     *
+     * @param int $length
+     * @return string
+     */
     public function createRandomName($length = 8)
     {
         $name = Menta_Util_Div::createRandomString($length, 'abcdefghijklmnopqrstuvwxyz');
         return ucfirst($name);
     }
-
-    public function createRandomString($length = 8, $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-    {
-        return substr(str_shuffle($chars), 0, $length);
-    }
-
-    /**
-     * Open registration page
-     */
-    public function openRegistrationPage()
-    {
-        $this->getHelperCommon()->open('/customer/account/create/');
-        $this->getHelperAssert()->assertBodyClass('customer-account-create');
-    }
-
 }
