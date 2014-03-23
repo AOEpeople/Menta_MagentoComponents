9a8118084f39db2c34af2727ec84d1f0dff9efc8
afirlejczyk <a.firlejczyk@macopedia.pl>
2014-03-23 18:29:17 +0100

    OnePageCheckout component - refactoring

--- lib/MagentoComponents/Pages/OnePageCheckout.php
+++ lib/MagentoComponents/Pages/OnePageCheckout.php
@@ -2,10 +2,93 @@
 
 class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTest
 {
+    /**
+     * Path for continue button in checkout
+     *
+     * @return string
+     */
+    protected function _getContinueButtonXPath()
+    {
+        return '//span[' . Menta_Util_Div::containsText($this->__('Continue')) . ']';
+    }
 
-    /*
+    /**
+     * Path for login button in checkout
+     * @return string
+     */
+    public function getLoginButtonPath()
+    {
+        return '//div[@id="checkout-step-login"]//span[contains(text(),"Login")]';
+    }
+
+    /**
+     * Path for place order button
+     *
+     * @return string
+     */
+    public function getPlaceOrderButtonPath()
+    {
+        return '//div[@id="checkout-review-submit"]//span[' .
+        Menta_Util_Div::containsText($this->__('Place Order')) . ']';
+    }
+
+    /**
+     * Path for checkbox "Save account for later use" (or other thing to click on)
+     *
+     * @return string
+     */
+    public function getSaveAccountCheckPath()
+    {
+        return "id=id_create_account";
+    }
+
+    /**
+     * Path for subtotal price in summary
+     *
+     * @return string
+     */
+    public function getSubtotalXpath()
+    {
+        return "//tfoot//td[". Menta_Util_Div::containsText('Subtotal') .
+        "]/following-sibling::td/span[" . Menta_Util_Div::contains('price') . "]";
+    }
+
+    /**
+     * Path for grand total price in summary
+     *
+     * @return string
+     */
+    public function getGrandTotalXpath()
+    {
+        return "//table[@id='checkout-review-table']/tfoot/tr[" .
+        Menta_Util_Div::contains('last') . "]//td[2]";
+    }
+
+    /**
+     * Path for flat rate shipping price in summary
+     *
+     * @return string
+     */
+    public function getShippingXpath()
+    {
+        return "//tfoot//td[". Menta_Util_Div::containsText('(Flat Rate - Fixed)') .
+        "]/following-sibling::td/span[" . Menta_Util_Div::contains('price') . "]";
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getTaxXpath()
+    {
+        return "//tfoot//td[". Menta_Util_Div::containsText('Tax') .
+        "]/following-sibling::td/span[" . Menta_Util_Div::contains('price') . "]";
+    }
+
+    /**
      * Go trough default magento checkout using default configuration and enable methods (shipping, payment)
      *
+     * @param bool $newsletter
      */
     public function goThroughCheckout($newsletter = false)
     {
@@ -38,6 +121,9 @@ class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTe
         $this->getHelperCommon()->click($this->getLoginButtonPath());
     }
 
+    /**
+     * Assert user is log in
+     */
     public function assertUserLogged()
     {
         $this->getHelperAssert()->assertElementNotPresent('//div[@class="step-title"]//h2[contains(text(),"' .
@@ -102,16 +188,6 @@ class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTe
     }
 
     /**
-     * Get continue button xpath
-     *
-     * @return string
-     */
-    protected function _getContinueButtonXPath()
-    {
-        return '//span[' . Menta_Util_Div::containsText($this->__('Continue')) . ']';
-    }
-
-    /**
      * Add shipping|billing address
      *
      * @param $country
@@ -138,11 +214,150 @@ class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTe
             $this->getHelperCommon()->type("id=$type:postcode", $address['postcode']);
             $this->getHelperCommon()->type("id=$type:company", $address['company']);
             $this->getHelperCommon()->select("id=$type:country_id", "label=" . $address['country']);
+
             if (isset($address['region']) && $address['region']) {
                 $this->getHelperCommon()->select("id=$type:region_id", "label=" . $address['region']);
             }
 
             return $address;
+        }
+    }
+
+    /**
+     * Enter valid visa credit card data
+     */
+    public function enterValidCreditCardDataVisa()
+    {
+        $this->getHelperWait()->waitForElementVisible('//input[@id="p_method_braintree"]');
+        $this->getHelperCommon()->click('//input[@id="p_method_braintree"]');
+        $this->getHelperWait()->waitForElementVisible('//ul[@id="payment_form_braintree"]');
+        $this->getHelperCommon()->select('id=braintree_cc_type', 'label=Visa');
+        $this->getHelperCommon()->type('id=braintree_cc_number', '4111111111111111');
+        $this->getHelperCommon()->select('id=braintree_expiration', 'label=03 - March');
+        $this->getHelperCommon()->select('id=braintree_expiration_yr', 'label=2020');
+        $this->getHelperCommon()->type('id=braintree_cc_cid', '123');
+    }
+
+    /**
+     * Submit order form
+     *
+     * @param bool $checkValidationPassed
+     * @param int $wait in seconds
+     */
+    public function submitForm($checkValidationPassed = TRUE, $wait = 1)
+    {
+        $this->getHelperAssert()->assertElementPresent($this->getPlaceOrderButtonPath());
+        $this->getHelperCommon()->click($this->getPlaceOrderButtonPath());
+        sleep($wait);
+        if ($checkValidationPassed) {
+            $this->getHelperAssert()->assertElementNotPresent("css=.validation-failed");
+            $this->getHelperAssert()->assertTextNotPresent("Please check red fields below and try again");
+            $this->getHelperWait()->waitForTextPresent($this->__('Sending your order'), 2);
+        }
+    }
+
+    /**
+     * Toggle ship to different address
+     */
+    public function toggleShipToDifferentAddress()
+    {
+        $this->getHelperAssert()->assertElementPresent('//input[@id="billing:use_for_shipping_no"]');
+        $this->getHelperCommon()->click('//input[@id="billing:use_for_shipping_no"]');
+    }
+
+    /**
+     * Get order id from order success page
+     * @return integer Order ID
+     */
+    public function getOrderIdFromSuccessPage()
+    {
+        $this->getHelperWait()->waitForElementPresent('//h1[contains(text(), "Your order has been received.")]');
+        $element = $orderNumber = $this->getHelperCommon()
+            ->getElement('//p[contains(text(),"Your order")]//a')->getAttribute('href');
+
+        $orderId = array_pop(explode('/', rtrim($element, '/')));
+
+        $this->getTest()->assertNotEmpty($orderId, 'No order id found!');
+
+        return $orderId;
+    }
+
+    /**
+     * Get order number from order success page
+     *
+     * @return mixed
+     */
+    public function getOrderNumberFromSuccessPage()
+    {
+        $this->getHelperWait()->waitForElementPresent('//h1[contains(text(), "Your order has been received.")]');
+        $orderNumber = $this->getHelperCommon()
+            ->getElement('//p[contains(text(),"Your order")]//a')->getText();
+        return $orderNumber;
+    }
+
+    /**
+     * Save account for latter use..
+     */
+    public function saveAccountForLaterUse()
+    {
+        $commonHelper = Menta_ComponentManager::get('Menta_Component_Helper_Common');
+        /* @var $commonHelper Menta_Component_Helper_Common */
+        $password = $this->getConfiguration()->getValue('testing.frontend.password');
+        $commonHelper->type('billing:customer_password', $password, true, true);
+        $commonHelper->type('billing:confirm_password', $password, true, true);
+    }
+
+    /**
+     * Select Flat Rate Shipping method
+     */
+    public function selectShippingFlatRate()
+    {
+        $this->selectShipping('Flat Rate');
+    }
+
+    /**
+     * Select any shipping method
+     *
+     * @param $name
+     */
+    public function selectShipping($name)
+    {
+        $this->getHelperCommon()->click("//td[@class='shipping-name']/label[text()='$name']");
+    }
+
+    /**
+     * Select Check / Money order payment method
+     */
+    public function selectPaymentMethodCheckmo()
+    {
+        $this->selectPaymentMethod('checkmo');
+    }
+
+    /**
+     * Select any payment method
+     * @param $code
+     */
+    public function selectPaymentMethod($code)
+    {
+        $this->getHelperCommon()->click("//dl[@id='checkout-payment-method-load']//input[@id='p_method_$code']");
+    }
+
+    /**
+     * Select newsletter checkbox
+     */
+    public function selectNewsletterCheckbox()
+    {
+        $this->getHelperCommon()->click("//input[@id='id_subscribe_newsletter']");
+    }
+
+    /**
+     * Assert Terms and Conditions
+     */
+    public function acceptTermsAndConditions()
+    {
+        $this->getHelperAssert()->assertElementPresent("//label[@for='id_accept_terms']", 'Could not find terms and conditions checkbox');
+        if (!$this->getHelperCommon()->isSelected("//input[@id='id_accept_terms']")) {
+            $this->getHelperCommon()->click("//input[@id='id_accept_terms']");
         }
     }
 
@@ -186,144 +401,86 @@ class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTe
         $this->getHelperWait()->waitForElementVisible('//*[@id="checkout-review-load"]');
     }
 
-    public function enterValidCreditCardDataVisa()
-    {
-        $this->getHelperWait()->waitForElementVisible('//input[@id="p_method_braintree"]');
-        $this->getHelperCommon()->click('//input[@id="p_method_braintree"]');
-        $this->getHelperWait()->waitForElementVisible('//ul[@id="payment_form_braintree"]');
-        $this->getHelperCommon()->select('id=braintree_cc_type', 'label=Visa');
-        $this->getHelperCommon()->type('id=braintree_cc_number', '4111111111111111');
-        $this->getHelperCommon()->select('id=braintree_expiration', 'label=03 - March');
-        $this->getHelperCommon()->select('id=braintree_expiration_yr', 'label=2020');
-        $this->getHelperCommon()->type('id=braintree_cc_cid', '123');
-    }
-
     /**
-     * Submit order form
+     * Assert product price in summary
      *
-     * @param bool $checkValidationPassed
+     * @param $count row number in table
+     * @param $expectedPrice
      */
-    public function submitForm($checkValidationPassed = TRUE)
+    public function assertPriceInSummary($count, $expectedPrice)
     {
-        $this->getHelperAssert()->assertElementPresent($this->getPlaceOrderButtonPath());
-        $this->getHelperCommon()->click($this->getPlaceOrderButtonPath());
-        sleep(1);
-        if ($checkValidationPassed) {
-            $this->getHelperAssert()->assertElementNotPresent("css=.validation-failed");
-            $this->getHelperAssert()->assertTextNotPresent("Please check red fields below and try again");
-            $this->getHelperWait()->waitForTextPresent($this->__('Sending your order'), 2);
-        }
-    }
-
-    public function toggleShipToDifferentAddress()
-    {
-        $this->getHelperAssert()->assertElementPresent('//input[@id="billing:use_for_shipping_no"]');
-        $this->getHelperCommon()->click('//input[@id="billing:use_for_shipping_no"]');
+        $price = $this->getHelperCommon()->getText('//tbody/tr[' .$count. ']/td[2]//span[@class="cart-price"]');
+        $this->getTest()->assertEquals(
+            $this->getHelper()->normalize($expectedPrice),
+            $this->getHelper()->normalize($price), 'Prices in summary is not as expected');
     }
 
     /**
-     * Get order id from order success page
-     * @return integer Order ID
-     */
-    public function getOrderIdFromSuccessPage()
-    {
-        /* @var $waitHelper Menta_Component_Helper_Wait */
-        $waitHelper = Menta_ComponentManager::get('Menta_Component_Helper_Wait');
-
-
-        $this->getHelperWait()->waitForElementPresent('//h1[contains(text(), "Your order has been received.")]');
-        $element = $orderNumber = $this->getHelperCommon()
-            ->getElement('//p[contains(text(),"Your order")]//a')->getAttribute('href');
-
-        $orderId = array_pop(explode('/', rtrim($element, '/')));
-
-        $this->getTest()->assertNotEmpty($orderId, 'No order id found!');
-        return $orderId;
-    }
-
-    /**
-     * Get order number from order success page
+     * Assert shipping price in summary
      *
-     * @return mixed
+     * @param $price
      */
-    public function getOrderNumberFromSuccessPage()
+    public function assertShippingPrice($price)
     {
-        $this->getHelperWait()->waitForElementPresent('//h1[contains(text(), "Your order has been received.")]');
-        $orderNumber = $this->getHelperCommon()
-            ->getElement('//p[contains(text(),"Your order")]//a')->getText();
-        return $orderNumber;
-    }
-
-    public function assertPriceInSummary($productId, $expectedPrice)
-    {
-        $price = $this->getHelperCommon()->getText('//tr[@id="product_' . $productId . '"]//td//div[@class="cart-price"]');
-        $price = strip_tags($price);
-        $this->getTest()->assertEquals($expectedPrice, $price, 'Prices in summary is not as expected');
-    }
-
-    public function saveAccountForLaterUse()
-    {
-        $commonHelper = Menta_ComponentManager::get('Menta_Component_Helper_Common');
-        /* @var $commonHelper Menta_Component_Helper_Common */
-        $password = $this->getConfiguration()->getValue('testing.frontend.password');
-        $commonHelper->type('billing:customer_password', $password, true, true);
-        $commonHelper->type('billing:confirm_password', $password, true, true);
+        /* @var Menta_Component_Helper_Common */
+        $actualPrice
+            = $this->getHelperCommon()->getText($this->getShippingXpath());
+        $this->getTest()->assertEquals(
+            $this->getHelper()->normalize($price),
+            $this->getHelper()->normalize($actualPrice),
+            'Shipping does not match'
+        );
     }
 
     /**
-     * Path for checkbox "Save account for later use" (or other thing to click on)
-     * @return string
+     * Assert subtotal in summary
+     *
+     * @param $expectedPrice
      */
-    public function getSaveAccountCheckPath()
+    public function assertSubtotal($expectedPrice)
     {
-        return "id=id_create_account";
-    }
-
-    public function assertShipping($expectedPrice)
-    {
-        $this->assertTotal($expectedPrice, 'shipping', 'Shipping price does not match');
-    }
-
-    public function assertTotal($expectedPrice, $type, $message = '')
-    {
-        $price = $this->getHelperCommon()->getText('//table[@id="checkout-review-totals-table"]//tr[' . GeneralComponents_Div::contains($type) . ']//td[' . GeneralComponents_Div::contains('value') . ']');
-        $price = strip_tags($price);
-        var_dump($expectedPrice . ' ' . $price . ';');
-        $this->getTest()->assertEquals($expectedPrice, $price, $message);
+        $price = $this->getHelperCommon()->getText($this->getSubtotalXpath());
+        $this->getTest()->assertEquals(
+            $this->getHelper()->normalize($price),
+            $this->getHelper()->normalize($expectedPrice),
+            'Subtotal does not match'
+        );
     }
 
     public function assertTax($expectedPrice)
     {
-        $this->assertTotal($expectedPrice, 'tax', 'Tax does not match');
+        $price = $this->getHelperCommon()->getText($this->getTaxXpath());
+        $this->getTest()->assertEquals(
+            $this->getHelper()->normalize($price),
+            $this->getHelper()->normalize($expectedPrice), 'Tax does not match');
     }
 
+    /**
+     * Assert grand total in summary
+     *
+     * @param $expectedPrice
+     */
     public function assertGrandTotal($expectedPrice)
     {
-        $this->assertTotal($expectedPrice, 'grand-total', 'Grand total does not match');
+        $price = $this->getHelperCommon()->getText($this->getGrandTotalXpath());
+
+        $this->getTest()->assertEquals(
+            $this->getHelper()->normalize($price),
+            $this->getHelper()->normalize($expectedPrice), 'Grand total does not match');
     }
 
-    public function assertSubtotal($expectedPrice)
-    {
-        $this->assertTotal($expectedPrice, 'subtotal', 'Subtotal does not match');
-    }
 
+    /**
+     * Assert billing country
+     * 
+     * @param $countryCode
+     */
     public function assertBillingCountry($countryCode)
     {
         $selected = $this->getHelperCommon()->getSelectedValue('//select[@id="billing:country_id"]');
         $this->getTest()->assertEquals($countryCode, $selected);
     }
 
-    /**
-     * Shipping price form middle column
-     * @param $name e.g. standard, priority...
-     * @param $price
-     */
-    public function assertShippingPrice($name, $price)
-    {
-        /* @var Menta_Component_Helper_Common */
-        $actualPrice = $this->getHelperCommon()->getText("//tr[@class='type_" . $name . "']/td[@class='shipping-price']");
-        $this->getTest()->assertEquals($price, $actualPrice);
-    }
 
     public function prepareShippingAddressFieldsForLoggedInUsers($conditionForOptionToSelect = "label=New Address")
     {
@@ -348,81 +505,14 @@ class MagentoComponents_Pages_OnePageCheckout extends Menta_Component_AbstractTe
         $this->getHelperCommon()->select("id=billing-address-select", $conditionForOptionToSelect);
     }
 
-    /**
-     * Assert Terms and Conditions
-     */
-    public function acceptTermsAndConditions()
-    {
-        $this->getHelperAssert()->assertElementPresent("//label[@for='id_accept_terms']", 'Could not find terms and conditions checkbox');
-        // $this->getHelperCommon()->click("id=id_accept_terms");
-        if (!$this->getHelperCommon()->isSelected("//input[@id='id_accept_terms']")) {
-            $this->getHelperCommon()->click("//input[@id='id_accept_terms']");
-        }
-    }
 
     /**
-     * Select Flat Rate Shipping method
-     */
-    public function selectShippingFlatRate()
-    {
-        $this->selectShipping('Flat Rate');
-    }
-
-    /**
-     * Select any shipping method
+     * Get Magento Helper
      *
-     * @param $name
+     * @return MagentoComponents_Helper
      */
-    public function selectShipping($name)
+    public function getHelper()
     {
-        $this->getHelperCommon()->click("//td[@class='shipping-name']/label[text()='$name']");
+        return Menta_ComponentManager::get('MagentoComponents_Helper');
     }
-
-    public function selectShippingMethodPriority()
-    {
-        $this->selectShipping('Priority');
-    }
-
-    /**
-     * Select Check / Money order payment method
-     */
-    public function selectPaymentMethodCheckmo()
-    {
-        $this->selectPaymentMethod('checkmo');
-    }
-
-    /**
-     * Select any payment method
-     * @param $code
-     */
-    public function selectPaymentMethod($code)
-    {
-        $this->getHelperCommon()->click("//dl[@id='checkout-payment-method-load']//input[@id='p_method_$code']");
-    }
-
-    public function selectNewsletterCheckbox()
-    {
-        $this->getHelperCommon()->click("//input[@id='id_subscribe_newsletter']");
-    }
-
-    /**
-     * Get XPath for login button in checkout
-     * @return string
-     */
-    public function getLoginButtonPath()
-    {
-        return '//div[@id="checkout-step-login"]//span[contains(text(),"Login")]';
-    }
-
-    /**
-     * Get Place Order button path
-     *
-     * @return string
-     */
-    public function getPlaceOrderButtonPath()
-    {
-        return '//div[@id="checkout-review-submit"]//span[' .
-        Menta_Util_Div::containsText($this->__('Place Order')) . ']';
-    }
-
 }
